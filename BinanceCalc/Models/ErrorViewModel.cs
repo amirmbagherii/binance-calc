7c146fd170a7997e0c4a3ac338424278af12f719
amirmbagheri23 <amir.m.mvc@gmail.com>
2022-07-31 16:51:04 +0430

    Upload Project

---
+++ BinanceCalc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BinanceCalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
